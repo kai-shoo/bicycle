0e25480475dc26ad9fb693307c57e8a85b035838
Kai Shoo <qsteamk@gmail.com>
2021-04-23 19:48:48 +0300

    Добавил первый экран — добавил блок формы — добавил  маску для телефона — добавил описание экрана

--- source/js/script.js
+++ source/js/script.js
@@ -9,3 +9,14 @@ header.classList.remove("header--active");
 toggle.addEventListener("click", () => {
   header.classList.toggle("header--active");
 });
+
+const phoneInput = document.querySelector('input[type="tel"');
+
+const phoneMask = IMask(phoneInput, {
+  mask: "+{7}(000)000-00-00",
+});
+
+const form = document.querySelector(".form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
