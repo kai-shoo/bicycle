9ef9fa9d0546972ebfeb21d20b4be50e5ac473b4
Kai Shoo <qsteamk@gmail.com>
2021-04-28 20:59:15 +0300

    Исправил ошибки связанные с критериями

--- source/js/script.js
+++ source/js/script.js
@@ -1,43 +1,45 @@
 "use strict";
 
-const header = document.querySelector(".header");
-const buttonToggle = document.querySelector(".toggle");
-const page = document.querySelector(".page");
+(function () {
+  const header = document.querySelector(".header");
+  const buttonToggle = document.querySelector(".toggle");
+  const page = document.querySelector(".page");
 
-if (header && buttonToggle && page) {
-  header.classList.remove("header--nojs");
-  header.classList.remove("header--active");
+  if (header && buttonToggle && page) {
+    header.classList.remove("header--nojs");
+    header.classList.remove("header--active");
 
-  const toggleMenu = (event) => {
-    if (event.target.closest(".toggle") || event.key === "Escape") {
-      header.classList.toggle("header--active");
-      page.classList.toggle("page--block");
-    }
+    const toggleMenu = (event) => {
+      if (event.target.closest(".toggle") || event.key === "Escape") {
+        header.classList.toggle("header--active");
+        page.classList.toggle("page--block");
+      }
 
-    if (header.classList.contains("header--active")) {
-      document.addEventListener("keydown", toggleMenu);
-    } else {
-      document.removeEventListener("keydown", toggleMenu);
-    }
-  };
+      if (header.classList.contains("header--active")) {
+        document.addEventListener("keydown", toggleMenu);
+      } else {
+        document.removeEventListener("keydown", toggleMenu);
+      }
+    };
 
-  buttonToggle.addEventListener("click", toggleMenu);
-}
+    buttonToggle.addEventListener("click", toggleMenu);
+  }
 
-const phoneInput = document.querySelector('input[type="tel"');
-const form = document.querySelector(".form");
+  const phoneInput = document.querySelector('input[type="tel"');
+  const form = document.querySelector(".form");
 
-if (phoneInput && form) {
-  const phoneMask = IMask(phoneInput, {
-    mask: "+{7}(000)000-00-00",
-  });
-
-  form.addEventListener("submit", (e) => {
-    const formData = new FormData(form);
-
-    fetch("https://echo.htmlacademy.ru/", {
-      method: "post",
-      body: formData,
+  if (phoneInput && form) {
+    const phoneMask = IMask(phoneInput, {
+      mask: "+{7}(000)000-00-00",
     });
-  });
-}
+
+    form.addEventListener("submit", (e) => {
+      const formData = new FormData(form);
+
+      fetch("https://echo.htmlacademy.ru/", {
+        method: "post",
+        body: formData,
+      });
+    });
+  }
+})();
