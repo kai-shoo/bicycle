33acf5d0fa87783bd160c9957f4a51b8ffd989b7
Kai Shoo <qsteamk@gmail.com>
2021-04-28 18:42:19 +0300

    Пиксель пёрфект для планшета и мобайл

--- source/js/script.js
+++ source/js/script.js
@@ -2,21 +2,27 @@
 
 const header = document.querySelector(".header");
 const toggle = document.querySelector(".toggle");
+const page = document.querySelector(".page");
 
-header.classList.remove("header--nojs");
-header.classList.remove("header--active");
+if (header && toggle && page) {
+  header.classList.remove("header--nojs");
+  header.classList.remove("header--active");
 
-toggle.addEventListener("click", () => {
-  header.classList.toggle("header--active");
-});
+  toggle.addEventListener("click", () => {
+    header.classList.toggle("header--active");
+    page.classList.toggle("page--block");
+  });
+}
 
 const phoneInput = document.querySelector('input[type="tel"');
 
-const phoneMask = IMask(phoneInput, {
-  mask: "+{7}(000)000-00-00",
-});
+if (phoneInput) {
+  const phoneMask = IMask(phoneInput, {
+    mask: "+{7}(000)000-00-00",
+  });
 
-const form = document.querySelector(".form");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
+  const form = document.querySelector(".form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+}
