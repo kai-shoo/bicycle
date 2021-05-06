7169e0f5e41d01de50f9ff3793354c8fbd7fe54d
Kai Shoo <qsteamk@gmail.com>
2021-05-06 16:49:25 +0300

    исправил неверное поведение формы при сабмите

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@
 ;
 (function () {
   const PHONE_INPUT = document.querySelector('input[type="tel"]');
-  const FORM = document.querySelector(".form");
+  const FORM = document.querySelector(".form__container");
 
   if (PHONE_INPUT && FORM) {
     const PHONE_MASK = IMask(PHONE_INPUT, {
@@ -44,6 +44,8 @@
     });
 
     FORM.addEventListener("submit", (e) => {
+      e.preventDefault();
+      console.log(e);
       const FORM_DATA = new FormData(FORM);
 
       fetch("https://echo.htmlacademy.ru/", {
@@ -52,5 +54,5 @@
       });
     });
   }
-})()
+})();
 ;
