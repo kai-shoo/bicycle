dba10ddc6205c792ad0f5125a44558d0c574249b
Kai Shoo <qsteamk@gmail.com>
2021-05-14 17:46:31 +0300

    исправлены замечения линтера

--- js/main.js
+++ js/main.js
@@ -1,56 +1,59 @@
 "use strict";
-
 (function () {
-  const HEADER = document.querySelector(".header");
-  const BUTTON_TOGGLE = document.querySelector(".toggle");
-  const PAGE = document.querySelector(".page");
+  const header = document.querySelector(`.header`);
+  const buttonToggle = document.querySelector(`.toggle`);
+  const page = document.querySelector(`.page`);
 
-  if (HEADER && BUTTON_TOGGLE && PAGE) {
-    PAGE.classList.remove("page--nojs");
-    HEADER.classList.remove("header--active");
+  if (header && buttonToggle && page) {
+    page.classList.remove(`page--nojs`);
+    header.classList.remove(`header--active`);
 
     const toggleMenu = (event) => {
       if (
-        (event.target.closest(".toggle") && event.type === "click") ||
-        event.key === "Escape"
+        (event.target.closest(`.toggle`) && event.type === `click`) ||
+        event.key === `Escape`
       ) {
-        HEADER.classList.toggle("header--active");
-        PAGE.classList.toggle("page--block");
+        header.classList.toggle(`header--active`);
+        page.classList.toggle(`page--block`);
       }
 
-      if (HEADER.classList.contains("header--active")) {
-        document.addEventListener("keydown", toggleMenu);
+      if (header.classList.contains(`header--active`)) {
+        document.addEventListener(`keydown`, toggleMenu);
       } else {
-        document.removeEventListener("keydown", toggleMenu);
+        document.removeEventListener(`keydown`, toggleMenu);
       }
     };
 
-    BUTTON_TOGGLE.addEventListener("click", toggleMenu);
-    HEADER.addEventListener("click", (event) => {
-      if (!event.target.classList.contains("nav__link")) return;
-      HEADER.classList.remove("header--active");
-      PAGE.classList.remove("page--block");
+    buttonToggle.addEventListener(`click`, toggleMenu);
+    header.addEventListener(`click`, (event) => {
+      if (!event.target.classList.contains(`nav__link`)) {
+        return;
+      }
+      header.classList.remove(`header--active`);
+      page.classList.remove(`page--block`);
     });
   }
 })();
 ;
+/* eslint-disable new-cap */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 (function () {
-  const PHONE_INPUT = document.querySelector('input[type="tel"]');
-  const FORM = document.querySelector(".form__container");
+  const phoneInput = document.querySelector(`input[type="tel"]`);
+  const form = document.querySelector(`.form__container`);
 
-  if (PHONE_INPUT && FORM) {
-    const PHONE_MASK = IMask(PHONE_INPUT, {
-      mask: "+{7}(000)000-00-00",
+  if (phoneInput && form) {
+    const phoneMask = IMask(phoneInput, {
+      mask: `+{7}(000)000-00-00`,
     });
 
-    FORM.addEventListener("submit", (e) => {
+    form.addEventListener(`submit`, (e) => {
       e.preventDefault();
-      console.log(e);
-      const FORM_DATA = new FormData(FORM);
+      const formData = new FormData(form);
 
-      fetch("https://echo.htmlacademy.ru/", {
-        method: "post",
-        body: FORM_DATA,
+      fetch(`https://echo.htmlacademy.ru/`, {
+        method: `post`,
+        body: formData,
       });
     });
   }
